68366b12b812791a6cb6d1f2b8443adf611ddcb9
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-17 13:57:50 -0700

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Jamie on 2018-08-17.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Jamie on 2018-08-17.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
